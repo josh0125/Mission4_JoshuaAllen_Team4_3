171090c159fbe10e5534e0f1caf5598e96c5287f
Joshua Allen <josh0125@byu.edu>
2023-02-01 12:55:44 -0700

    semicolons

--- Mission4_JoshuaAllen_Team4_3/wwwroot/js/JavaScript.js
+++ Mission4_JoshuaAllen_Team4_3/wwwroot/js/JavaScript.js
@@ -12,34 +12,34 @@ $("#buttomSub").click(function () {
     var intex = $('#intex').val();
 
     // Calculating Grade
-    var total = (assignment * .5) + (gproject * .1) + (quizzes * .1) + (midterm * .1) + (final * .1) + (intex * .1)
-    var grade = ""
+    var total = (assignment * .5) + (gproject * .1) + (quizzes * .1) + (midterm * .1) + (final * .1) + (intex * .1);
+    var grade = "";
 
     // Determining Numeric Grade
     if (total >= 94)
-        grade = 'A'
+        grade = 'A';
     else if (total >= 90)
-        grade = 'A-'
+        grade = 'A-';
     else if (total >= 87)
-        grade = 'B+'
+        grade = 'B+';
     else if (total >= 84)
-        grade = 'B'
+        grade = 'B';
     else if (total >= 80)
-        grade = 'B-'
+        grade = 'B-';
     else if (total >= 77)
-        grade = 'C+'
+        grade = 'C+';
     else if (total >= 74)
-        grade = 'C'
+        grade = 'C';
     else if (total >= 70)
-        grade = 'C-'
+        grade = 'C-';
     else if (total >= 67)
-        grade = 'D+'
+        grade = 'D+';
     else if (total >= 64)
-        grade = 'D'
+        grade = 'D';
     else if (total >= 60)
-        grade = 'D-'
+        grade = 'D-';
     else
-        grade = 'E'
+        grade = 'E';
 
     // Displaying results to empty paragraph on DOM
     $('#results').text('Numeric Grade: ' + total + '%\nLetter Grade: ' + grade)
