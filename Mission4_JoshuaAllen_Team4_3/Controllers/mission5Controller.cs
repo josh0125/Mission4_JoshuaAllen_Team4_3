8137f545022bfde6beb1a1e5b9dfd62129e377d1
Joshua Allen <josh0125@byu.edu>
2023-02-04 17:08:41 -0700

    Mission 5 - 1

---
+++ Mission4_JoshuaAllen_Team4_3/Controllers/mission5Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_JoshuaAllen_Team4_3.Controllers
+{
+    public class mission5Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+    }
+}
